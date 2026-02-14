5241b1500cb92ec7378389519a26f0095f1ea749
Adam Čisár <adamcisar7@gmail.com>
2026-02-14 09:36:02 +0100

    add soft deletes to todos table

--- app/Features/Todo/Models/Todo.php
+++ app/Features/Todo/Models/Todo.php
@@ -10,11 +10,13 @@ use Illuminate\Database\Eloquent\Attributes\UseResourceCollection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[UseResourceCollection(TodoResourceCollection::class)]
 class Todo extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'todos';
 
